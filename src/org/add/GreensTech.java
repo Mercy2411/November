85edadfb0004ded8e8e3b1d3f78b5211bc857b2b
Navis@12345* <mercyedvin24@gmail.com>
2021-12-14 11:05:56 +0530

    first

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class GreensTech {
+private void greensOmr() {
+	System.out.println("GreensOmr address is:NO:19 mahabalipuram Rd Opposite to Sankeetha hotel Thoraipakkam chennai-600097");
+
+}
+private void greensAdyar() {
+	System.out.println("GreensAdyar address is :4th Floor, JK Pritzker, SDV Arcade, AB-5, 2nd Ave, AB Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu"
+			+ "");
+}
+	private void greensTambaram() {
+		System.out.println("GreensTambaram address is :No.1, Apparao Colony, Tambaram Sanatorium, Chennai, Tamil Nadu 600047");
+
+	}
+	private void greensVelachery() {
+		System.out.println("GreensVelachery address is :NO 28, Nagendra Nagar, Opposite Phoenix Mall, Velachery Main Road, Velachery, Chennai, Tamil Nadu 600042");
+	}
+private void greensAnnaNagar() {
+
+System.out.println("GreensAnnaNagar address is :97/4, 2nd Ave, AB Block, Shanthi Colony, Anna Nagar, Chennai");
+}
+public static void main(String[] args) {
+	GreensTech g = new GreensTech();
+	g.greensOmr();
+	g.greensAdyar();
+	g.greensTambaram();
+	g.greensVelachery();
+	g.greensAnnaNagar();
+			
+
+
+}
+}
